e306266932aee26bc921d63a1b75354cf993fdf7
NoDocCat <nodoccat@outlook.com>
2021-03-02 00:10:44 +0800

    build: 构建清理改用 shx 执行并取消构建过程中的压缩插件

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,4 @@
 import typescript from "rollup-plugin-typescript2";
-import { terser } from "rollup-plugin-terser";
-import cleaner from "rollup-plugin-cleaner";
 
 export default {
   input: "./src/index.ts",
@@ -9,11 +7,5 @@ export default {
     { dir: "./dist", entryFileNames: "index.cjs.js", format: "cjs", sourcemap: true },
   ],
   external: ["rxjs", "rxjs/operators"],
-  plugins: [
-    typescript({
-      useTsconfigDeclarationDir: true,
-    }),
-    terser(),
-    cleaner({ targets: ["./dist/"] }),
-  ],
+  plugins: [typescript({ useTsconfigDeclarationDir: true })],
 };
