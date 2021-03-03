93b3e8691bdc1c913cd38506090265bcd63b18bb
NoDocCat <nodoccat@outlook.com>
2021-03-03 21:33:05 +0800

    build: 调整包入口点和构建配置

--- rollup.config.js
+++ rollup.config.js
@@ -1,10 +1,11 @@
 import typescript from "rollup-plugin-typescript2";
+import pkg from "./package.json";
 
 export default {
   input: "./src/index.ts",
   output: [
-    { dir: "./dist", entryFileNames: "index.js", format: "es", sourcemap: true },
-    { dir: "./dist", entryFileNames: "index.cjs.js", format: "cjs", sourcemap: true },
+    { file: pkg.main, format: "cjs", sourcemap: true },
+    { file: pkg.module, format: "es", sourcemap: true },
   ],
   external: ["rxjs", "rxjs/operators"],
   plugins: [typescript({ useTsconfigDeclarationDir: true })],
