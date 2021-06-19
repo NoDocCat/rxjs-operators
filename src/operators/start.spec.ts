665f0c2190a4daf95a1a92393695504b074a890c
NoDocCat <nodoccat@outlook.com>
2021-06-19 23:31:58 +0800

    test: 重新设计测试用例

--- src/operators/start.spec.ts
+++ src/operators/start.spec.ts
@@ -1,84 +1,37 @@
 import { of } from "rxjs";
-import { finalize } from "rxjs/operators";
 import { TestScheduler } from "rxjs/testing";
 import { start } from "./start";
 
-describe("operators/start", () => {
-  it("操作符应不影响流的内容", () => {
-    const testScheduler = new TestScheduler((a, b) => expect(a).toEqual(b));
+test("操作符应不影响流的内容", () => {
+  const testScheduler = new TestScheduler((a, b) => expect(a).toEqual(b));
 
-    testScheduler.run(helpers => {
-      const { cold, expectObservable } = helpers;
-      const source = cold("-a--b---c---|").pipe(start(() => null));
-      expectObservable(source).toBe("-a--b---c---|");
-    });
+  testScheduler.run(helpers => {
+    const { cold, expectObservable } = helpers;
+    const source = cold("-a--b---c---|").pipe(start(() => null));
+    expectObservable(source).toBe("-a--b---c---|");
   });
+});
 
-  it("副作用应在订阅时执行", done => {
-    let loading = false;
+test("应在订阅时执行副作用", () => {
+  const fn = jest.fn();
+  const source = of(1).pipe(start(fn));
 
-    const source = of(1).pipe(start(() => (loading = true)));
+  expect(fn).toBeCalledTimes(0);
 
-    expect(loading).not.toBeTruthy();
+  source.subscribe();
+  expect(fn).toBeCalledTimes(1);
+});
 
-    source.subscribe(() => {
-      try {
-        expect(loading).toBeTruthy();
-        done();
-      } catch (e) {
-        done(e);
-      }
-    });
-  });
+test("应只执行一次副作用", done => {
+  const fn = jest.fn();
+  const source = of(1, 2, 3).pipe(start(fn));
 
-  it("副作用应只执行一次", done => {
-    let count = 0;
+  expect(fn).toBeCalledTimes(0);
 
-    const source = of(1, 2, 3).pipe(start(() => (count += 1)));
-
-    source.subscribe({
-      complete: () => {
-        try {
-          expect(count).toBe(1);
-          done();
-        } catch (e) {
-          done(e);
-        }
-      },
-    });
-  });
-
-  it("应正确执行清理函数", done => {
-    let loading = false;
-    let running = false;
-
-    const source = of(1).pipe(
-      finalize(() => {
-        try {
-          expect(loading).not.toBeTruthy();
-          expect(running).not.toBeTruthy();
-          done();
-        } catch (e) {
-          done(e);
-        }
-      }),
-      start(() => {
-        loading = true;
-        return () => (loading = false);
-      }),
-      start(() => {
-        running = true;
-        return () => (running = false);
-      })
-    );
-
-    source.subscribe(() => {
-      try {
-        expect(loading).toBeTruthy();
-        expect(running).toBeTruthy();
-      } catch (e) {
-        done(e);
-      }
-    });
+  source.subscribe({
+    complete: () => {
+      expect(fn).toBeCalledTimes(1);
+      done();
+    },
   });
 });
